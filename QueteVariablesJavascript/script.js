822eee32b1fe325b7b1443e58747d0bc65de7e5a
Chaymae El Hayani <layani.chay@gmail.com>
2024-03-04 16:05:40 +0100

    Add files via upload

---
+++ QueteVariablesJavascript/script.js
@@ -0,0 +1,35 @@
+
+  /*
+    🐦 DAVID Bruno Twitter profile 🐦
+
+    🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
+    variable keyword you have learned in the quest.
+    ex: const name = "David";
+
+  */
+
+    const Name = "David"; 
+    const lastName = "Bruno"; 
+    
+    let davidBiography = "Hi, I'm David Bruno from SF, I like to cook and meet new people."; 
+
+
+    console.log(davidBiography);
+  
+  
+    let davidProfilePicture = "http://www.go.com/davif.png";
+  
+    let cityCounty = "San Francisco";
+  
+    let followers = "109"; 
+    let following = "200";
+  
+  // 🏁 Exercise 02 - David is following one more account increment the total of following account
+
+following = "201";
+
+console.log("I'm folowing " + following +" accounts");
+console.log( "I have " + followers + " folowers");
+
+
+
